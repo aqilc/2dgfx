9abe5f9b9d2053a1224d84b4e35ce687b6c724ca
Aqil Contractor <aqilcm@gmail.com>
2023-12-16 16:39:23 -0500

    New demo featuring tons of dynamically sized moving text in the background. Need to optimize better.

--- main.c
+++ main.c
@@ -1,39 +1,50 @@
-
-#include <stdio.h>
-
+#include <stdlib.h>
+#include <math.h>
 
 // My custom graphics lib :3
 #define GFX_MAIN_DEFINE
 #include <2dgfx.h>
 #include <vec.h>
 
-// int main(void) {
-//   gfx_init("2dgfx", 800, 600);
-//   img hi = gfx_loadimg("hyperAngery.png");
-//   typeface fon = gfx_loadfont("roboto.ttf");
-//   char* fps = NULL;
-  
-//   while(gfx_nextframe()) {
-//     gfx_vector m = gfx_mouse();
-//     fill(255, 255, 255, 255);
-//     rect(m.x, m.y, 20, 20);
-//     image(hi, 350, 200, 100, 100);
-//     fontsize(20);
-//     text("hello! THIS IS FINALLY WORKING PROPERLY WOOOOOO eahubrancodikr", 10, 400);
-//     text("AAAAAAAAAAAA YES YOU YOU DON'T DISTORT ANYMOER", 10, 430);
-//     fontsize(200);
-//     text("BIG", 50, 300);
 
-//     if(gfx_fpschanged())
-//       fps = vfmt("%.2f fps", gfx_fps());
-//     fontsize(20);
-//     if(fps) text(fps, 800 - 150, 600 - 10);
-    
-//     gfx_frameend();
-//   }
+char* messages[] = {
+  "lol",
+  "ez",
+  "cool demo",
+  "but CAN YOU DO THIS?",
+  "send n*des",
+  "lmao"
+};
 
-//   return 0;
-// }
+#define MLEN (sizeof(messages) / sizeof(messages[0]))
+
+struct text_particle {
+  gfx_vector pos;
+  int size;
+  int text;
+  float spd;
+  unsigned char col;
+} particles[500];
+
+#define PLEN (sizeof(particles) / sizeof(particles[0]))
+
+void particle_reset(int id, int height) {
+  particles[id].pos.fx = -(rand() / (float) RAND_MAX) * 300 + 100;
+  particles[id].spd    =  (rand() / (float) RAND_MAX) * 5.f + 1.f;
+  particles[id].pos.y  =  (rand() / (float) RAND_MAX) * (height + 50);
+  particles[id].size   =  (rand() / (float) RAND_MAX) * 45 + 5;
+  particles[id].text   =  (rand() % MLEN);
+  particles[id].col    =  (rand() / (float) RAND_MAX) * 200 + 55;
+}
+void particles_draw(int width, int height) {
+  for(int i = 0; i < PLEN; i ++) {
+    if(particles[i].pos.fx > width + 100) particle_reset(i, height);
+    particles[i].pos.fx += particles[i].spd;
+    fontsize(particles[i].size);
+    fill(particles[i].col, particles[i].col, particles[i].col, 255);
+    text(messages[particles[i].text], particles[i].pos.fx, particles[i].pos.y);
+  }
+}
 
 img hi;
 typeface fon;
@@ -42,9 +53,16 @@ void setup() {
   gfx_init("2dgfx", 800, 600);
   hi = gfx_loadimg("hyperAngery.png");
   fon = gfx_loadfont("roboto.ttf");
+
+  for(int i = 0; i < PLEN; i ++)
+    particle_reset(i, 600);
 }
 void loop() {
   gfx_vector m = gfx_mouse();
+  gfx_vector dims = gfx_screen_dims();
+
+
+  particles_draw(dims.w, dims.h);
   fill(255, 255, 255, 255);
   rect(m.x, m.y, 20, 20);
   image(hi, 350, 200, 100, 100);
