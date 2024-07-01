03b34f3d03d7a4106064ae708cc61fb6ff6d1fb7
Aqil Contractor <aqilcm@gmail.com>
2024-07-01 03:02:52 -0400

    [https] Add another test testing web requests.

--- tests/ts.bat
+++ tests/ts.bat
@@ -5,7 +5,7 @@ rem Test Standalone
 
 mkdir bin
 set c=clang-cl --target=i386-pc-win32 /c /MDd /Z7 /EHsc /Od /D_CRT_SECURE_NO_WARNINGS /I"../deps/include/" /I"../lib/" /Fo"./bin/" %1.c
-for /f "tokens=*" %%F in ('dir /b /a:-d "..\deps\*.c"') do call set c=%%c%% "..\deps\%%F"
+rem for /f "tokens=*" %%F in ('dir /b /a:-d "..\deps\*.c"') do call set c=%%c%% "..\deps\%%F"
 echo running %c%
 %c%
 lld-link freetype.lib glew32s.lib Gdi32.lib user32.lib shell32.lib opengl32.lib glfw3.lib zlib.lib msvcrt.lib ./bin/*.obj /nologo /DEBUG:FULL /ignore:4099 /MACHINE:X86 /LIBPATH:"../deps/lib/" /NODEFAULTLIB:libcmt.lib /OUT:test.exe /SUBSYSTEM:CONSOLE
@@ -15,5 +15,5 @@ cd ..
 rmdir bin
 
 test
-del test.exe
+rem del test.exe
 del test.pdb
