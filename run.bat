570514e5e133d8cb7b3e96e4a5219a53ca0aaa45
Aqil Contractor <aqilcm@gmail.com>
2025-05-15 01:22:37 -0400

    fix(build): Update run.bat to support new config

--- run.bat
+++ run.bat
@@ -15,16 +15,19 @@ setlocal EnableDelayedExpansion
 mkdir bin
 :: set c=clang-cl --target=i386-pc-win32 /c /MD /EHsc /O2 /D_CRT_SECURE_NO_WARNINGS /I"./deps/include/" /I"./lib/" /Fo"./bin/" main.c
 :: set c=clang-cl --target=i386-pc-win32 /c /MDd /Z7 /EHsc /Od /D_CRT_SECURE_NO_WARNINGS /I"./deps/include/" /I"./lib/" /Fo"./bin/" main.c
-set c=clang-cl --target=i386-pc-win32 /c /MD /Zi /EHsc /O2 /D_CRT_SECURE_NO_WARNINGS /I"./deps/include/" /I"./lib/" /Fo"./bin/" main.c
+:: set c=clang-cl --target=i386-pc-win32 /c /MD /Zi /EHsc /O2 /D_CRT_SECURE_NO_WARNINGS /I"./deps/include/" /I"./lib/" /Fo"./bin/" main.c
+:: set c=clang-cl --target=x86_64-pc-win32 /c /MD /Zi /EHsc /O2 /D_CRT_SECURE_NO_WARNINGS /I"./deps/include/" /I"./lib/" /Fo"./bin/" main.c
+set c=clang-cl --target=x86_64-pc-win32 /c /MDd /Z7 /EHsc /Od /D_CRT_SECURE_NO_WARNINGS /DDEBUG /I"./deps/include/" /I"./lib/" /Fo"./bin/" main.c
 :: for /f "tokens=*" %%F in ('dir /b /a:-d "*.c"') do call set c=%%c%% "%%F"
-for /f "tokens=*" %%F in ('dir /b /a:-d ".\deps\*.c"') do call set c=%%c%% ".\deps\%%F"
+:: for /f "tokens=*" %%F in ('dir /b /a:-d ".\deps\*.c"') do call set c=%%c%% ".\deps\%%F"
 for /f "tokens=*" %%F in ('dir /b /a:-d ".\lib\*.c"') do call set c=%%c%% ".\lib\%%F"
-set c=%c% /DTRACY_ENABLE .\deps\extern\tracy\TracyClient.cpp
+:: set c=%c% /DTRACY_ENABLE .\deps\extern\tracy\TracyClient.cpp
 echo running %c%
 %c%
 
 :: DEBUG:
-lld-link freetype.lib glew32s.lib Gdi32.lib user32.lib ntdll.lib shell32.lib opengl32.lib glfw3.lib msvcrtd.lib ./bin/*.obj /nologo /DEBUG:FULL /ignore:4099 /MACHINE:X86 /LIBPATH:"./deps/lib/" /NODEFAULTLIB:libcmt.lib /OUT:main.exe /SUBSYSTEM:CONSOLE
+:: lld-link freetype.lib glew32s.lib Gdi32.lib user32.lib ntdll.lib shell32.lib opengl32.lib glfw3.lib msvcrtd.lib ./bin/*.obj /nologo /DEBUG:FULL /ignore:4099 /MACHINE:X86 /LIBPATH:"./deps/lib/" /NODEFAULTLIB:libcmt.lib /OUT:main.exe /SUBSYSTEM:CONSOLE
+lld-link freetype.lib glew.lib Gdi32.lib user32.lib ntdll.lib shell32.lib opengl32.lib glfw3.lib msvcrtd.lib ./bin/*.obj /nologo /DEBUG:FULL /ignore:4099 /MACHINE:X64 /LIBPATH:"./deps/lib/64bit" /NODEFAULTLIB:libcmt.lib /OUT:main.exe /SUBSYSTEM:CONSOLE
 
 :: PROD:
 :: lld-link freetype.lib glew32s.lib Gdi32.lib user32.lib shell32.lib opengl32.lib glfw3.lib msvcrt.lib ./bin/*.obj /nologo /DEBUG:FULL /ignore:4099 /MACHINE:X86 /LIBPATH:"./deps/lib/" /NODEFAULTLIB:libcmt.lib /OUT:main.exe /SUBSYSTEM:CONSOLE
